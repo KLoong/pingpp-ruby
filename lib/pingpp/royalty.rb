4f7d193483392c4bdac61482544a654b8a6018a4
allen chen <liu.xiuxiu@puscene.com>
2021-12-17 13:40:38 +0800

    add royalty

--- lib/pingpp/royalty.rb
+++ lib/pingpp/royalty.rb
@@ -10,5 +10,15 @@ module Pingpp
     def self.resource_url(opt={})
       '/v1/royalties'
     end
+
+    def self.create(id, params, opts={})
+      response, opts = request(:post, create_url(id), params, opts)
+      Util.convert_to_pingpp_object(response, opts)
+    end
+
+    def self.create_url order_id
+      "/v1/orders/#{order_id}/royalty_datas"
+    end
+
   end
 end
